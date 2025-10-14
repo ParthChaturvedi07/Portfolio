0fd7987c86005fbc4f908d27dcb6dc91b1d4230a
Parth Chaturvedi <155241893+ParthChaturvedi07@users.noreply.github.com>
2025-10-14 18:53:15 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -292,7 +292,7 @@ export const Header = () => {
           <Resume_Button
             onClick={() =>
               window.open(
-                "https://drive.google.com/file/d/1j7p69yvD7NqU3yzuy8at3vLej7KT5eT9/view?usp=sharing",
+                "https://drive.google.com/file/d/1YOOr7sKdhcrrnW9fZN5P8p8VKFnk0fBH/view?usp=sharing",
                 "_blank"
               )
             }
@@ -339,7 +339,7 @@ export const Header = () => {
             className="button"
             onClick={() =>
               window.open(
-                "https://drive.google.com/file/d/1j7p69yvD7NqU3yzuy8at3vLej7KT5eT9/view?usp=sharing",
+                "https://drive.google.com/file/d/1YOOr7sKdhcrrnW9fZN5P8p8VKFnk0fBH/view?usp=sharing",
                 "_blank"
               )
             }
